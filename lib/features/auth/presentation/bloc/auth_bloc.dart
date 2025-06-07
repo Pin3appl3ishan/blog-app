a23323b71986676a7d1bc1a2d6a55ce2d96e8d8f
Pin3appl3ishan <sthaishan2004@gmail.com>
2025-06-07 18:11:03 +0545

    implement AuthBloc with UserSignUp use case for handling user registration

--- lib/features/auth/presentation/bloc/auth_bloc.dart
+++ lib/features/auth/presentation/bloc/auth_bloc.dart
@@ -1,13 +1,25 @@
-import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
+import 'package:blog_app/features/auth/domain/repository/usecases/user_sign_up.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'auth_event.dart';
 part 'auth_state.dart';
 
 class AuthBloc extends Bloc<AuthEvent, AuthState> {
-  AuthBloc() : super(AuthInitial()) {
-    on<AuthEvent>((event, emit) {
-      // TODO: implement event handler
+  final UserSignUp _userSignUp;
+  AuthBloc({required UserSignUp userSignup})
+    : _userSignUp = userSignup,
+      super(AuthInitial()) {
+    on<AuthSignUp>((event, emit) async {
+      final res = await _userSignUp(
+        UserSignUpParams(
+          name: event.name,
+          email: event.email,
+          password: event.password,
+        ),
+      );
+
+      res.fold((l) => emit(AuthFailure(l.message)), (r) => emit(AuthSuccess(r)));
     });
   }
 }
