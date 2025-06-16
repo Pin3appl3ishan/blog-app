49fe784419ec624f547b7cd6e4c1b34c87f8c62b
Pin3appl3ishan <sthaishan2004@gmail.com>
2025-06-16 22:51:52 +0545

    feat: add optional posterName field to BlogModel and Blog entity

--- lib/features/blog/data/models/blog_model.dart
+++ lib/features/blog/data/models/blog_model.dart
@@ -9,6 +9,7 @@ class BlogModel extends Blog {
     required super.imageUrl,
     required super.topics,
     required super.updatedAt,
+    super.posterName
   });
 
   Map<String, dynamic> toJson() {
@@ -56,6 +57,7 @@ class BlogModel extends Blog {
       imageUrl: imageUrl ?? this.imageUrl,
       topics: topics ?? this.topics,
       updatedAt: updatedAt ?? this.updatedAt,
+      posterName: posterName ?? this.posterName
     );
   }
 }
