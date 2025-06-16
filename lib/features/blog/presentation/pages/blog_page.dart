c23676bfb64d7434a50c6d8a3a9485bbc8f868e9
Pin3appl3ishan <sthaishan2004@gmail.com>
2025-06-16 22:55:08 +0545

    feat: refactor BlogPage to use stateful widget and implement BlogsDisplaySuccess handling

--- lib/features/blog/presentation/pages/blog_page.dart
+++ lib/features/blog/presentation/pages/blog_page.dart
@@ -1,9 +1,24 @@
+import 'package:blog_app/core/common/widgets/loader.dart';
+import 'package:blog_app/core/utils/show_snackbar.dart';
+import 'package:blog_app/features/blog/presentation/bloc/blog_bloc.dart';
 import 'package:blog_app/features/blog/presentation/pages/add_new_blog_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class BlogPage extends StatelessWidget {
+class BlogPage extends StatefulWidget {
   static route() => MaterialPageRoute(builder: (context) => const BlogPage());
   const BlogPage({super.key});
+
+  @override
+  State<BlogPage> createState() => _BlogPageState();
+}
+
+class _BlogPageState extends State<BlogPage> {
+  @override
+  void initState() {
+    super.initState();
+    context.read<BlogBloc>().add(GetAllBlogsEvent());
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,6 +33,28 @@ class BlogPage extends StatelessWidget {
             icon: const Icon(Icons.add_circle_outline_outlined),
           ),
         ],
+      ),
+      body: BlocConsumer<BlogBloc, BlogState>(
+        listener: (context, state) {
+          if (state is BlogFailure) {
+            showSnackBar(context, state.error);
+          }
+        },
+        builder: (context, state) {
+          if (state is BlogLoading) {
+            return const Loader();
+          }
+          if (state is BlogsDisplaySuccess) {
+            return ListView.builder(
+              itemCount: state.blogs.length,
+              itemBuilder: (context, index) {
+                final blog = state.blogs[index];
+                return Text(blog.title);
+              },
+            );
+          }
+          return const SizedBox();
+        },
       ),
     );
   }
